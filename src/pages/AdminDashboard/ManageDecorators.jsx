a934cebface6183e7440bcdcd4cb035879bbc590
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2025-12-06 19:09:42 +0600

    displayed user data from database

--- src/pages/AdminDashboard/ManageDecorators.jsx
+++ src/pages/AdminDashboard/ManageDecorators.jsx
@@ -1,4 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../hooks/useAuth";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+
 const ManageDecorators = () => {
+    const {user} = useAuth();
+    const axiosSecure = useAxiosSecure();
+    const {data: users = [], isLoading, refetch} = useQuery({
+        queryKey: ['users', user?.email],
+        queryFn: async () => {
+            const result = await axiosSecure(`/users`);
+            return result.data;
+        }
+    });
     return (
         <div className="space-y-4">
             <h2 className="text-4xl font-bold">Manage Decorators</h2>
@@ -6,41 +19,38 @@ const ManageDecorators = () => {
                 <table className="table">
                     <thead>
                         <tr>
-                            <th></th>
+                            <th>SL</th>
                             <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Email</th>
+                            <th>Role</th>
                             <th></th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th></th>
+                        {users.map((user, index) => <tr key={user._id}>
+                            <th>{index + 1}</th>
                             <td>
                                 <div className="flex items-center gap-3">
                                     <div className="avatar">
                                         <div className="mask mask-squircle h-12 w-12">
                                             <img
-                                                src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                                                alt="Avatar Tailwind CSS Component" />
+                                                src={user.photoURL}
+                                                alt={user.displayName} />
                                         </div>
                                     </div>
                                     <div>
-                                        <div className="font-bold">Hart Hagerty</div>
-                                        <div className="text-sm opacity-50">United States</div>
+                                        <div className="font-bold">{user.displayName}</div>
                                     </div>
                                 </div>
                             </td>
                             <td>
-                                Zemlak, Daniel and Leannon
-                                <br />
-                                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                                {user.email}
                             </td>
-                            <td>Purple</td>
+                            <td>{user.role}</td>
                             <th>
-                                <button className="btn btn-ghost btn-xs">details</button>
+                                <button className="btn btn-primary text-black">Make Decorator</button>
                             </th>
-                        </tr>
+                        </tr>)}
                     </tbody>
                 </table>
             </div>
