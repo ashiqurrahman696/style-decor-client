0505ecf6b6d60445e9404dcea945ccc7ef8f380c
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2025-12-06 16:31:46 +0600

    created footer section

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/shared/Navbar";
 import { ToastContainer } from "react-toastify";
+import Footer from "../components/shared/Footer";
 
 const MainLayout = () => {
     return (
         <div>
             <Navbar/>
             <Outlet/>
+            <Footer/>
             <ToastContainer/>
         </div>
     );
