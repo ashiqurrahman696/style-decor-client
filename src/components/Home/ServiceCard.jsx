ade64560bf58b114952857a0e77dbfd3a951fa54
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2025-12-07 17:56:09 +0600

    updated service card

--- src/components/Home/ServiceCard.jsx
+++ src/components/Home/ServiceCard.jsx
@@ -1,11 +1,17 @@
 const ServiceCard = ({service}) => {
     return (
         <div className="card bg-base-200 border border-neutral-300">
+            <figure className="px-4 pt-4">
+                <img
+                    src={service.packageImgURL}
+                    alt={service.service_name}
+                    className="rounded-xl" />
+            </figure>
             <div className="card-body">
                 <h2 className="card-title">{service.service_name}</h2>
                 <p>{service.description}</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary text-white">Buy Now</button>
+                <div className="card-actions">
+                    <button className="btn btn-primary text-white">View Details</button>
                 </div>
             </div>
         </div>
