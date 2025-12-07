75769395f5e881dac31d625cd4261e8dab424186
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2025-12-07 20:46:23 +0600

    created service coverage map section in homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Coverage from "../../components/Home/Coverage";
 import Services from "../../components/Home/Services";
 
 const Home = () => {
     return (
-        <div className="max-w-7xl mx-auto p-4">
+        <div className="max-w-7xl mx-auto p-4 space-y-4">
             <Services/>
+            <Coverage/>
         </div>
     );
 };
