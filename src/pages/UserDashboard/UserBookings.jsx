19f6fea13b7772f63a003f54099e4f5b78e657f4
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2025-12-08 17:31:04 +0600

    added pay, update and cancel button + implemented booking update functionality

--- src/pages/UserDashboard/UserBookings.jsx
+++ src/pages/UserDashboard/UserBookings.jsx
@@ -1,10 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../hooks/useAuth";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useRef, useState } from "react";
+import { toast } from "react-toastify";
 
 const UserBookings = () => {
+    const [selectedBooking, setSelectedBooking] = useState(null);
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
+    const updateModalRef = useRef();
     const { data: userBookings = [], isLoading, refetch } = useQuery({
         queryKey: ['userBookings', user?.email],
         queryFn: async () => {
@@ -12,6 +16,26 @@ const UserBookings = () => {
             return result.data;
         }
     });
+
+    const openUpdateBooking = booking => {
+        setSelectedBooking(booking);
+        updateModalRef.current.showModal();
+    }
+
+    const handleUpdateBooking = e => {
+        e.preventDefault();
+        const bookingDate = e.target.booking_date.value;
+        const updatedBooking = {
+            booking_date: bookingDate
+        };
+        axiosSecure.patch(`/booking/${selectedBooking._id}`, updatedBooking)
+            .then(res => {
+                if(res.data.modifiedCount){
+                    refetch();
+                    toast.success("Booking updated successfully");
+                }
+            });
+    }
     return (
         <div className="space-y-4">
             <h2 className="text-4xl font-bold">My Bookings</h2>
@@ -40,11 +64,40 @@ const UserBookings = () => {
                             <td>{booking.cost}</td>
                             <td>{new Date(booking.created_at).toLocaleString()}</td>
                             <td>{booking.payment_status}</td>
-                            <td>{booking.payment_status === "unpaid" && <button className="btn btn-primary text-black">Pay</button>}</td>
+                            <td>
+                                <div className="flex gap-2">
+                                    {booking.payment_status === "unpaid" && <button className="btn btn-primary text-black">Pay</button>}
+                                    <button onClick={() => openUpdateBooking(booking)} className="btn btn-secondary text-white">Update</button>
+                                    <button className="btn btn-primary text-black">Cancel</button>
+                                </div>
+                            </td>
                         </tr>)}
                     </tbody>
                 </table>
             </div>
+            <dialog ref={updateModalRef} className="modal">
+                <div className="modal-box w-11/12 max-w-5xl">
+                    {/* content */}
+                    <div className="space-y-4">
+                        <h2 className="text-4xl font-bold">Update Booking</h2>
+                        <form onSubmit={handleUpdateBooking} className="grid md:grid-cols-2 gap-4">
+                            <div>
+                                <label className="label">Booking Date</label>
+                                <input type="date" name="booking_date" defaultValue={selectedBooking?.booking_date} className="input w-full" />
+                            </div>
+                            <div className="col-span-full">
+                                <button className="btn btn-primary text-black">Update</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            {/* if there is a button, it will close the modal */}
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
         </div>
     );
 };
