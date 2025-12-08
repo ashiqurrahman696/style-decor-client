84ad882490dd1a2cd38573b0c2dd58c5c48fcae7
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2025-12-08 09:58:48 +0600

    created animated hero section using framer-motion in homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Coverage from "../../components/Home/Coverage";
+import Hero from "../../components/Home/Hero";
 import Services from "../../components/Home/Services";
 
 const Home = () => {
     return (
         <div className="max-w-7xl mx-auto p-4 space-y-4">
+            <Hero/>
             <Services/>
             <Coverage/>
         </div>
