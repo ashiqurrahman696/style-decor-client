4e58978d68751f9146c2f4826c8d5aaccffad4e0
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2025-12-08 20:18:06 +0600

    implemented pagination functionality in manage decorators page

--- src/pages/AdminDashboard/ManageDecorators.jsx
+++ src/pages/AdminDashboard/ManageDecorators.jsx
@@ -2,15 +2,21 @@ import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../hooks/useAuth";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { toast } from "react-toastify";
+import { useState } from "react";
 
 const ManageDecorators = () => {
     const {user} = useAuth();
     const axiosSecure = useAxiosSecure();
+    const [totalPage, setTotalPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState(0);
+    const limit = 10;
     const {data: users = [], isLoading, refetch} = useQuery({
-        queryKey: ['users', user?.email],
+        queryKey: ['users', user?.email, currentPage],
         queryFn: async () => {
-            const result = await axiosSecure(`/users`);
-            return result.data;
+            const result = await axiosSecure(`/users?limit=${limit}&skip=${currentPage * limit}`);
+            const page = Math.ceil(result.data.total / limit);
+            setTotalPage(page);
+            return result.data.result;
         }
     });
 
@@ -73,6 +79,22 @@ const ManageDecorators = () => {
                         </tr>)}
                     </tbody>
                 </table>
+            </div>
+            <div className="flex justify-center flex-wrap gap-3 py-10">
+                {
+                    currentPage > 0 &&
+                    <button onClick={() => setCurrentPage(currentPage - 1)} className="btn btn-primary text-black">&lt; Prev</button>
+
+                }
+                {
+                    [...Array(totalPage).keys()].map((i) =>
+                        <button onClick={() => setCurrentPage(i)} className={`btn ${i === currentPage && "btn-primary text-black"}`}>{i}</button>
+                    )
+                }
+                {
+                    currentPage < totalPage - 1 &&
+                    <button onClick={() => setCurrentPage(currentPage + 1)} className="btn btn-primary text-black">Next &gt;</button>
+                }
             </div>
         </div>
     );
