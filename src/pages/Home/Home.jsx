bfc4bf7ef9adf92908d0e4e854726a1572620cc3
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2026-01-09 14:48:40 +0600

    created about section in homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import About from "../../components/Home/About";
 import Coverage from "../../components/Home/Coverage";
 import Hero from "../../components/Home/Hero";
 import Services from "../../components/Home/Services";
@@ -7,6 +8,7 @@ const Home = () => {
     return (
         <div className="max-w-7xl mx-auto p-4 space-y-4">
             <Hero/>
+            <About/>
             <Services/>
             <TopDecorators/>
             <Coverage/>
