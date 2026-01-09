b5ab3c697d2c12b95f944748e503448a7dfb9f96
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2026-01-09 11:42:54 +0600

    implemented search and sort functionality in all services page

--- src/components/Home/ServiceCard.jsx
+++ src/components/Home/ServiceCard.jsx
@@ -7,10 +7,11 @@ const ServiceCard = ({service}) => {
                 <img
                     src={service.packageImgURL}
                     alt={service.service_name}
-                    className="rounded-xl" />
+                    className="rounded-xl w-full h-40 object-cover" />
             </figure>
             <div className="card-body">
                 <h2 className="card-title">{service.service_name}</h2>
+                <p>Tk. {service.cost}</p>
                 <p>{service.description}</p>
                 <div className="card-actions">
                     <Link to={`/service/${service._id}`} className="btn btn-primary text-black">View Details</Link>
