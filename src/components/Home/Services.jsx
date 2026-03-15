274bc181ef3e3926ec2eea0fafa478644201c252
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2026-03-15 20:04:28 +0600

    set service card number from 3 to 4 in homepage's services section

--- src/components/Home/Services.jsx
+++ src/components/Home/Services.jsx
@@ -8,7 +8,7 @@ const Services = () => {
         queryKey: ['serviceData'],
         queryFn: async () => {
             const result = await axios(`/services`);
-            return result.data.slice(0, 3);
+            return result.data.slice(0, 4);
         }
     });
     return (
