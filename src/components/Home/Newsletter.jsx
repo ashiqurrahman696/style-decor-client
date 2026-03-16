a813b51dde509499d4ff1a07220eee5e4c6a6642
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2026-03-16 08:44:02 +0600

    modified newsletter section in homepage

--- src/components/Home/Newsletter.jsx
+++ src/components/Home/Newsletter.jsx
@@ -1,13 +1,15 @@
 const Newsletter = () => {
     return (
         <div>
-            <div className="card bg-base-300">
-                <div className="card-body space-y-5 items-center py-20">
-                    <h1 className="text-center font-semibold text-4xl">Subscribe to our Newsletter</h1>
-                    <form className="w-full max-w-xl join">
-                        <input className="input join-item w-full" placeholder="Email" />
-                        <button className="btn join-item">Subscribe</button>
-                    </form>
+            <div className="card bg-[url(https://i.ibb.co.com/zVzQmgJR/La-Importancia-de-la-Decoracion-en-un-Evento-Corporativo.jpg)] bg-cover bg-center">
+                <div className="bg-black/40 inset-0">
+                    <div className="card-body space-y-5 items-center py-20 text-white">
+                        <h1 className="text-center font-semibold text-4xl">Subscribe to our Newsletter</h1>
+                        <form className="w-full max-w-xl join">
+                            <input className="input join-item w-full outline-none text-black" placeholder="Email" />
+                            <button className="btn join-item btn-primary text-black">Subscribe</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
