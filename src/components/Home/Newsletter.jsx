48921860e94e11ca82b87e72af386c8aa53f44aa
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2026-03-16 14:31:06 +0600

    fixed ui of homepage's newsletter section

--- src/components/Home/Newsletter.jsx
+++ src/components/Home/Newsletter.jsx
@@ -3,10 +3,10 @@ const Newsletter = () => {
         <div>
             <div className="card bg-[url(https://i.ibb.co.com/zVzQmgJR/La-Importancia-de-la-Decoracion-en-un-Evento-Corporativo.jpg)] bg-cover bg-center overflow-hidden">
                 <div className="bg-black/40 inset-0">
-                    <div className="card-body space-y-5 items-center py-20 text-white">
-                        <h1 className="text-center font-semibold text-4xl">Subscribe to our Newsletter</h1>
+                    <div className="card-body space-y-5 items-center py-20">
+                        <h1 className="text-center font-semibold text-4xl text-white">Subscribe to our Newsletter</h1>
                         <form className="w-full max-w-xl join">
-                            <input className="input join-item w-full outline-none text-black" placeholder="Email" />
+                            <input className="input join-item w-full outline-none" placeholder="Email" />
                             <button className="btn join-item btn-primary text-black">Subscribe</button>
                         </form>
                     </div>
